d94b2d6424214ccdb346fc99e71103046b681cfc
Shine Wang <shine.sw.wang@gmail.com>
2019-07-21 18:48:17 -0400

    bug fix

--- 15/15-6.c
+++ 15/15-6.c
@@ -8,7 +8,8 @@ int main(int __attribute__((unused)) argc, char *argv[]) {
   const char *file = argv[1];
   struct stat stats;
   stat(file, &stats);
-  mode_t to_set = S_IRUSR | S_IRGRP | S_IROTH;
+  mode_t to_set = stats.st_mode;
+  to_set |= S_IRUSR | S_IRGRP | S_IROTH;
   if ((stats.st_mode & S_IFMT) == S_IFDIR || (stats.st_mode & S_IXUSR) ||
       (stats.st_mode & S_IXGRP) || (stats.st_mode & S_IXOTH)) {
     to_set |= S_IXUSR | S_IXGRP | S_IXOTH;
