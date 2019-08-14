be1449d948a76adbe48b4e2c7a8f2c4a9be814e0
Shine Wang <shine.sw.wang@gmail.com>
2019-08-14 00:59:35 -0400

    22-1.c

---
+++ 22/22-1.c
@@ -0,0 +1,29 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void handler(int sig) {
+  printf("got signal %d\n", sig);
+}
+
+int main() {
+  sigset_t signals;
+  sigemptyset(&signals);
+  sigaddset(&signals, SIGCONT);
+  sigprocmask(SIG_BLOCK, &signals, NULL);
+  struct sigaction act;
+  act.sa_flags = 0;
+  sigset_t blocked;
+  sigemptyset(&blocked);
+  act.sa_mask = blocked;
+  act.sa_flags = 0;
+  act.sa_handler = handler;
+  sigaction(SIGCONT, &act, NULL);
+  sigaction(SIGUSR1, &act, NULL);
+  pause();
+  sigprocmask(SIG_UNBLOCK, &signals, NULL);
+  pause();
+  return 0;
+}
